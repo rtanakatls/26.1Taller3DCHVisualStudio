f7e5f604cea9f692db1ef9993a496e9b74b21bd3
Ricardo Tanaka Terukina <rtanaka@talento.tls.edu.pe>
2026-03-26 17:12:08 -0500

    Created project and example

---
+++ Menu.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3DCH261
+{
+    internal class Menu
+    {
+
+        public void Execute()
+        {
+            string name;
+            int age;
+
+            
+            name = GetName();
+            Console.WriteLine($"Hola {name}");
+
+            age = GetAge();
+            Console.WriteLine($"Tienes {age} años");
+            Console.WriteLine($"El próximo año, tendrás {age + 1}");
+        }
+
+        private string GetName()
+        {
+            string name = "";
+            while (name.Length == 0)
+            {
+                Console.WriteLine("Introduce tu nombre");
+                name = Console.ReadLine();
+                if(name.Length==0)
+                {
+                    Console.WriteLine("Debes introducir un nombre");
+                }
+            }
+            return name;
+        }
+
+        private int GetAge()
+        {
+            int age = -1;
+            while (age < 0)
+            {
+                Console.WriteLine("Introduce tu edad:");
+                age=int.Parse( Console.ReadLine() );
+                if (age < 0)
+                {
+                    Console.WriteLine("La edad no puede ser negativa");
+                }
+            }
+            return age;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3DCH261
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            menu.Execute();
+        }
+    }
+}
