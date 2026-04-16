2338d5dd231e8850e14d10899fddcd8fdfb38f5e
Ricardo Tanaka Terukina <rtanaka@talento.tls.edu.pe>
2026-04-16 16:32:23 -0500

    Added enemy

---
+++ Enemy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3DCH261
+{
+    internal class Enemy
+    {
+    }
+}
